0bbd093008b7c33e831418e3feef9d8e54587939
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-04-27 02:14:16 +0000

    create a contact js and css and the skeleton for the contact js

---
+++ src/front/js/pages/contact.js
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/contact.css";
+
+export const Contact = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="layer-one container-fluid" id="work-body">
+      <div className="main">
+        <div className="container text-center">
+          <div className="row align-items-center">
+            <div className=" contact-links col-1">
+              <div className="contact-left-link">
+                <p>O__/2024</p>
+              </div>
+              <p>______________</p>
+              <p className="home">
+                <a href="/">HOME</a>
+              </p>
+            </div>
+            <div className="contact col">
+              <h2 className="contact-title mb-3">HELLO.</h2>
+              <p>
+                Looking for a stunning, meticulously crafted website that you
+                can manage and update independently? Get in touch.
+              </p>
+            </div>
+            <div className="work-right-column col-8"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
