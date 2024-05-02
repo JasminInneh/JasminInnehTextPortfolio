02ae77af39ae7f2c60403b15f64c5e571f949c95
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-05-02 17:04:36 +0000

    finally adjusted the home mobile view to my liking

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,7 +5,6 @@ import "../../styles/home.css";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
-  
   return (
     <div className="layer-one container-fluid" id="home-body">
       <div className="main animate__animated animate__fadeInDown">
@@ -31,26 +30,26 @@ export const Home = () => {
               </p>
             </div>
             {/* Mobile navigation bar */}
-          <div className="d-lg-none d-block">
-            <div className="mobile-nav">
-              <p>O__/2024</p>
-              <p>______________</p>
-              <p className="github">
-                <a href="https://github.com/jasmininneh">GITHUB</a>
-              </p>
-              <p>
-                <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">
-                  LINKEDIN
-                </a>
-              </p>
-              <p>
-                <a href="https://gpt-3-application-zeta.vercel.app/">
-                  PROJECTS
-                </a>
-              </p>
+            <div className="d-lg-none d-block">
+              <div className="mobile-nav">
+                <p>O__/2024</p>
+                <p>______________</p>
+                <p className="github">
+                  <a href="https://github.com/jasmininneh">GITHUB</a>
+                </p>
+                <p>
+                  <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">
+                    LINKEDIN
+                  </a>
+                </p>
+                <p>
+                  <a href="https://gpt-3-application-zeta.vercel.app/">
+                    PROJECTS
+                  </a>
+                </p>
+              </div>
             </div>
-          </div>
-            <div className="home-intro col">
+            <div className="home-intro col-lg">
               <h5 className="name mb-3">Jasmin Nicole Inneh</h5>
               <p>Full Stack Developer | React | JavaScript | Python |MySQL</p>
               <p>
@@ -58,7 +57,7 @@ export const Home = () => {
                 Florida.
               </p>
             </div>
-            <div className="home-right-column col-8">
+            <div className="home-right-column col-lg">
               <h1 className="large-text display-1 home-large-text">
                 <a href="/Work">WORK</a>
               </h1>
@@ -74,4 +73,74 @@ export const Home = () => {
       </div>
     </div>
   );
-};
+  
+};  
+//   return (
+//     <div className="layer-one container-fluid" id="home-body">
+//       <div className="main animate__animated animate__fadeInDown">
+//         <div className="container text-center">
+//           <div className="row align-items-center">
+//             <div className="home-links col-1">
+//               <div className="home-left-link">
+//                 <p>O__/2024</p>
+//               </div>
+//               <p>______________</p>
+//               <p className="github">
+//                 <a href="https://github.com/jasmininneh">GITHUB</a>
+//               </p>
+//               <p>
+//                 <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">
+//                   LINKEDIN
+//                 </a>
+//               </p>
+//               <p>
+//                 <a href="https://gpt-3-application-zeta.vercel.app/">
+//                   PROJECTS
+//                 </a>
+//               </p>
+//             </div>
+//             {/* Mobile navigation bar */}
+//           <div className="d-lg-none d-block">
+//             <div className="mobile-nav">
+//               <p>O__/2024</p>
+//               <p>______________</p>
+//               <p className="github">
+//                 <a href="https://github.com/jasmininneh">GITHUB</a>
+//               </p>
+//               <p>
+//                 <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">
+//                   LINKEDIN
+//                 </a>
+//               </p>
+//               <p>
+//                 <a href="https://gpt-3-application-zeta.vercel.app/">
+//                   PROJECTS
+//                 </a>
+//               </p>
+//             </div>
+//           </div>
+//             <div className="home-intro col">
+//               <h5 className="name mb-3">Jasmin Nicole Inneh</h5>
+//               <p>Full Stack Developer | React | JavaScript | Python |MySQL</p>
+//               <p>
+//                 Creating beautiful and engaging digital experiences in Miami,
+//                 Florida.
+//               </p>
+//             </div>
+//             <div className="home-right-column col-8">
+//               <h1 className="large-text display-1 home-large-text">
+//                 <a href="/Work">WORK</a>
+//               </h1>
+//               <h1 className="large-text display-1 home-large-text">
+//                 <a href="/About">ABOUT</a>
+//               </h1>
+//               <h1 className="large-text display-1 home-large-text">
+//                 <a href="/Contact">CONTACT</a>
+//               </h1>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
